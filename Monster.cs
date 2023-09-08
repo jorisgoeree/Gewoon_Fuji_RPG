71f59bc19ee9041804fd44c367f8529bc99c72ea
jorisgoeree <joris_05@hotmail.com>
2023-09-08 09:36:56 +0200

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -4,13 +4,15 @@
     public string Name;
     public int MaxHitPoints;
     public int MaxDamage;
+    public int UnknownVariable; // TO DO figure out what to use this variable for
 
-    public Monster(int id, string name, int maxDamage, int maxHitPoints)
+    public Monster(int id, string name, int maxDamage, int maxHitPoints, int unknownVariable)
     {
         ID = id;
         Name = name;
         MaxHitPoints = maxHitPoints;
         MaxDamage = maxDamage;
+        UnknownVariable = unknownVariable;
         
     }
 }
